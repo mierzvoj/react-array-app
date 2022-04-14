fabb6ab104a2f8e2da7d34e59a3fb8e95ea53a20
mierzvoj <mierzvoj@gmail.com>
2022-04-14 21:23:49 +0200

    first working commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      const element = (
+      <div>
+        <h1>Witaj świecie!</h1>
+        <h2>Aktualny czas: {new Date().toLocaleTimeString()}.</h2>
+      </div>
+      );
     </div>
   );
 }
