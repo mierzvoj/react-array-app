5b486dfa24e2168ada992296fb9bb94bade54b4d
mierzvoj <mierzvoj@gmail.com>
2022-04-18 19:29:18 +0200

    not working commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+
 const arr = [];
+arr.length = 10;
 const numbers = [];
 const App = () => {
-  const getRandom = () => Math.floor(Math.random() * 20) + 1;
+  const getRandom = () => Math.floor(Math.random() * 10);
 
-  for (let i = 0; i < 10; i++) {
+  for (let k = 0; k < 10; k++) {
     arr.push(getRandom());
   }
 
   console.log(arr);
-  const listItems = arr.map((numbers) => <li>{numbers}</li>);
-  return <ul>{listItems}</ul>;
+  const listItems = arr.map((numbers, i) => (
+    <li key={`numbers -${i}`}> {numbers}</li>
+  ));
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  return (
+    <ul>
+      {listItems}
+      <button onClick={refreshPage}> Refresh me </button>
+    </ul>
+  );
 };
 export default App;
 
